da438eb38dd85fbaa45d9a3093a972be46ff0f26
Medini Chaitanya <medinichaitanya@gmail.com>
2024-08-11 12:21:19 -0600

    add logging to app.py and fix aws lambda issues

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 import os
 import time
-from flask import Flask, jsonify, request
 import requests
 import boto3
+import logging
+from flask import Flask, jsonify, request
 from boto3.dynamodb.conditions import Key
 from dotenv import load_dotenv
+
+logging.basicConfig(
+  filename='app.log',
+  level=logging.DEBUG,
+  format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+)
 
 app = Flask(__name__)
 
@@ -16,22 +23,29 @@ APIFY_INDEED_KEY = os.getenv('APIFY_INDEED_KEY')
 
 @app.route('/indeed_jobs', methods=['POST'])
 def get_indeed_jobs():
-  api_url = f'https://api.apify.com/v2/acts/misceres~indeed-scraper/run-sync-get-dataset-items?token={APIFY_INDEED_KEY}'
-  payload = request.json
+  logging.info('Received request to scape jobs from Indeed API')
+  try:
+    api_url = f'https://api.apify.com/v2/acts/misceres~indeed-scraper/run-sync-get-dataset-items?token={APIFY_INDEED_KEY}'
+    payload = request.json
 
-  response = requests.post(api_url, json=payload)
-  data = response.json()
+    response = requests.post(api_url, json=payload)
+    data = response.json()
+    logging.info(f'data from first url is {data}')
 
-  time.sleep(30)
+    time.sleep(30)
 
-  dataset_url= f'https://api.apify.com/v2/datasets/{data}/items?token={APIFY_INDEED_KEY}'
-  result_response = requests.get(dataset_url)
-  result_data = result_response.json()
+    dataset_url= f'https://api.apify.com/v2/datasets/{data}/items?token={APIFY_INDEED_KEY}'
+    result_response = requests.get(dataset_url)
+    result_data = result_response.json()
+    logging.info(f'this is the data from result_data - {result_data}')
 
-  for item in result_data:
-    table.put_item(Item=item)
+    for item in result_data:
+      table.put_item(Item=item)
 
-  return jsonify(status='success', data=data)
+    return jsonify(status='success', data=result_data)
+  except Exception as e:
+    logging.error(f'Error scraping jobs {e}')
+    return jsonify(error='Failed to scrape jobs'), 500
 
 @app.route('/', methods=['GET'])
 def hello_world():
